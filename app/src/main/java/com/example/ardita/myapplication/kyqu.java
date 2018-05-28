a8c38daff8f6a46b4e44d397b37b9f98d81c4e5f
LauraSvarca <laureta.svarca@studentet.uni-pr.edu>
2018-05-28 15:30:05 +0200

    test2

--- app/src/main/java/com/example/ardita/myapplication/kyqu.java
+++ app/src/main/java/com/example/ardita/myapplication/kyqu.java
@@ -10,21 +10,36 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.widget.*;
 public class kyqu extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     String[] Drejtimi = { "Shkenca natyrore", "Shkenca shoqerore", "Mjeksia", "Shkolla teknike" };
+    CheckBox matematike,fizike,biologji,gjuheshqipe,gjuheangleze,kimi;
+    Button vazhdo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kyqu);
-        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        Spinner spin = (Spinner) findViewById(R.id.Spinner);
         spin.setOnItemSelectedListener(this);
+        addListenerOnButtonClick();
 
         //Creating the ArrayAdapter instance having the country list
         ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,Drejtimi);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         spin.setAdapter(aa);
+    }
+
+    private void addListenerOnButtonClick() {
+        matematike=(CheckBox)findViewById(R.id.lenda1);
+        fizike=(CheckBox)findViewById(R.id.lenda2);
+        biologji=(CheckBox)findViewById(R.id.lenda3);
+        gjuheshqipe=(CheckBox)findViewById(R.id.lenda4);
+        gjuheangleze=(CheckBox)findViewById(R.id.lenda5);
+        kimi=(CheckBox)findViewById(R.id.lenda6);
+        vazhdo=(Button)findViewById(R.id.vazhdo);
     }
 
 
